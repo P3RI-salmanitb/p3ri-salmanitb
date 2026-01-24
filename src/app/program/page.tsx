28af95ece94e0cdf3fbe81c2d2b6defd15a571c9
muthiariesta <marsarchivee@gmail.com>
2026-01-24 15:21:50 +0700

    style: merapikan css untuk versi responsive

--- src/app/program/page.tsx
+++ src/app/program/page.tsx
@@ -1,15 +1,15 @@
 import ProgramCard from "../../components/features/ProgramCard";
 import { PROGRAMS_DATA } from "../../lib/constants";
 
-const ProgramPage = () => {
+export default function ProgramPage() {
   return (
-      <main className="min-h-screen bg-[#F7FFD8] py-10 sm:py-14 md:py-16 lg:py-20 px-4 sm:px-6 md:px-12 lg:px-20">
+    <main className="min-h-screen bg-[#F7FFD8] py-10 sm:py-14 md:py-16 lg:py-20 px-4 md:px-12 lg:px-20">
       <div className="max-w-360 mx-auto">
-        <h1 className="text-[#353B00] font-forum text-2xl sm:text-3xl md:text-4xl lg:text-[56px] font-normal mb-8 md:mb-10 lg:mb-12 text-center">
+        <h1 className="text-[#353B00] font-forum text-3xl md:text-4xl lg:text-[56px] font-normal mb-8 md:mb-10 lg:mb-12 text-center">
           Program P3RI
         </h1>
-        
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
+
+        <div className="p-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 sm:gap-6 md:gap-8">
           {PROGRAMS_DATA.map((program) => (
             <ProgramCard
               key={program.id}
@@ -24,5 +24,3 @@ const ProgramPage = () => {
     </main>
   );
 }
-
-export default ProgramPage
--- src/components/features/ProgramCard.tsx
+++ src/components/features/ProgramCard.tsx
@@ -1,5 +1,4 @@
 import Link from "next/link";
-import { ArrowRight } from "lucide-react"; 
 
 interface ProgramCardProps {
   slug: string;
@@ -11,22 +10,24 @@ interface ProgramCardProps {
 const ProgramCard = ({ slug, title, date_display, summary }: ProgramCardProps) => {
   return (
     <Link href={`/program/${slug}`} className="block h-full">
-      <div className="bg-white rounded-2xl md:rounded-3xl p-4 sm:p-5 md:p-6 shadow-2xl border-2 border-white hover:border-[#ADCD61] transition-all flex flex-col h-full">
-        <h3 className="text-[--color-text-body] font-montserrat text-lg sm:text-xl font-bold mb-2">
+      <div className="bg-white rounded-2xl md:rounded-3xl p-4 md:p-6 shadow-2xl border-2 border-white hover:border-[#ADCD61] transition-all flex flex-col h-full">
+        <h3 className="text-[#21272A] font-montserrat text-lg md:text-xl font-bold mb-1 md:mb-2">
           {title}
         </h3>
         
-        <p className="text-[--color-text-body] font-montserrat text-sm sm:text-base font-semibold mb-3 sm:mb-4">
+        <p className="text-[#21272A] font-montserrat text-base md:text-lg font-semibold mb-4 md:mb-5">
           {date_display}
         </p>
         
-        <p className="text-[--color-text-body] font-montserrat text-sm sm:text-base font-medium mb-4 sm:mb-6 flex-1">
+        <p className="text-[#21272A] font-montserrat text-sm md:text-base font-medium mb-5 md:mb-6 flex-1">
           {summary}
         </p>
         
-        <div className="flex items-center gap-4 text-[#8F9F00] font-montserrat text-sm sm:text-base font-semibold">
+        <div className="flex items-center gap-2 text-[#8F9F00] font-montserrat text-sm sm:text-base font-semibold">
           More Info
-            <ArrowRight size={18} strokeWidth={2.5} className="sm:w-5 sm:h-5" />
+          <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="w-4h-4">
+            <path d="M5 12h14M12 5l7 7-7 7"/>
+          </svg>
         </div>
       </div>
     </Link>
